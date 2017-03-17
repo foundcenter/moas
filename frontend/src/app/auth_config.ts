2f54cc105ea63d8afa1f45fb96e667edbe9522c9
Nebojsa Vojvodic <neb.vojvodic@gmail.com>
2017-03-17 13:05:15 +0100

    Configured Github oauth popup

--- frontend/src/app/auth_config.ts
+++ frontend/src/app/auth_config.ts
@@ -4,6 +4,9 @@ import {CustomConfig} from 'ng2-ui-auth';
  */
 export const GOOGLE_CLIENT_ID = '531545236739-hhtfh2m5rcmeph76sabo3mvdupeu5hfa.apps.googleusercontent.com';
 export const SLACK_CLIENT_ID = '16724837808.155634746450';
+export const GITHUB_CLIENT_ID = '469a838ef4c6048510b6';
+
+export const BACKEND_MOCK = 'https://httpbin.org/post';
 
 export class MyAuthConfig extends CustomConfig {
     defaultHeaders = {'Content-Type': 'application/json'};
@@ -16,7 +19,7 @@ export class MyAuthConfig extends CustomConfig {
         },
         slack: {
             clientId: SLACK_CLIENT_ID,
-            url: "https://httpbin.org/post",
+            url: BACKEND_MOCK,
             authorizationEndpoint: 'https://slack.com/oauth/authorize',
             requiredUrlParams: ['scope'],
             redirectUri: 'http://localhost:4200',
@@ -25,6 +28,18 @@ export class MyAuthConfig extends CustomConfig {
             scopeDelimiter: ',',
             oauthType: '2.0',
             popupOptions: { width: 700, height: 800 },
-        }
+        },
+        github: {
+            clientId: GITHUB_CLIENT_ID,
+            name: 'github',
+            url: BACKEND_MOCK,
+            authorizationEndpoint: 'https://github.com/login/oauth/authorize',
+            redirectUri: 'http://localhost:4200',
+            requiredUrlParams: ['scope'],
+            scope: ['user:email'],
+            scopeDelimiter: ' ',
+            oauthType: '2.0',
+            popupOptions: {width: 1020, height: 720}
+        },
     };
 }
