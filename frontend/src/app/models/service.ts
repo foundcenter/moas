0c0d12897a925f608522b1a6d7860393ea0fcf07
Nebojsa Vojvodic <neb.vojvodic@gmail.com>
2017-03-20 16:11:58 +0100

    Minor reformat

--- frontend/src/app/models/service.ts
+++ frontend/src/app/models/service.ts
@@ -6,25 +6,29 @@ export class Service {
   constructor(public name: string, public logo: string, public info: string) {
   }
 
-  getLogoUrl(): string{
-    return 'assets/images/'+this.logo+'_logo.png';
+  getLogoUrl(): string {
+    return 'assets/images/' + this.logo + '_logo.png';
   }
 
   static JIRA(): Service {
     return new Service('Jira', 'jira', 'Search projects and issues.');
   }
+
   static GMAIL(): Service {
     return new Service('Gmail', 'gmail', 'Search messages, threads and attachments.');
   }
+
   static GOOGLEDRIVE(): Service {
     return new Service('Google Drive', 'google_drive', 'Search your drive files and directories.');
   }
+
   static SLACK(): Service {
     return new Service('Slack', 'slack', 'Search direct messages, channels and attachments.');
   }
+
   static GITHUB(): Service {
     return new Service('GitHub', 'github', 'Search commits, repositories and issues.');
   }
 
 
-  }
+}
