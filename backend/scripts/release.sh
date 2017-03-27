604538eea1f929ca44a6f92cd7a26b359aedb536
Aleksandar Mirilovic <amirilovic@gmail.com>
2017-03-27 17:18:19 +0200

    Adding docker build

---
+++ backend/scripts/release.sh
@@ -0,0 +1,1 @@
+docker push fcid/moas:latest
--- backend/scripts/run.sh
+++ backend/scripts/run.sh
@@ -1,1 +1,1 @@
-docker run -it -v ${PWD}:/go/src/github.com/foundcenter/moas/backend/ --env-file .env.production -p 8081:8081 --name moas --rm moas:latest
+docker run -it -v ${PWD}:/go/src/github.com/foundcenter/moas/backend/ --env-file .env.production -p 8080:8080 --name moas --rm fcid/moas:latest
