f8628445e1e7c2bbf4f7e4a1cbc61556ec81311d
Aleksandar Mirilovic <amirilovic@gmail.com>
2017-03-27 17:51:08 +0200

    Changing google client id

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   apiUrl: 'https://fluent-ego-162813.appspot.com',
   redirectUrl: 'http://localhost:4200',
-  googleClientId: '531545236739-hhtfh2m5rcmeph76sabo3mvdupeu5hfa.apps.googleusercontent.com',
+  googleClientId: '115655419099-5irupagjg158brcr5o0ief540dm7gg01.apps.googleusercontent.com',
   slackClientId: '16724837808.155634746450',
   githubClientId: '469a838ef4c6048510b6'
 };
