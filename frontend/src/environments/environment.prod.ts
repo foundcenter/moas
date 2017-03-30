d3be5493676a9dce9d4c4bdffaf53e820c01f4ee
Aleksandar Mirilovic <amirilovic@gmail.com>
2017-03-30 13:57:41 +0200

    Update google client id

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   apiUrl: 'https://fci-moas.herokuapp.com',
   redirectUrl: 'http://moas.foundcenter.com',
-  googleClientId: '115655419099-5irupagjg158brcr5o0ief540dm7gg01.apps.googleusercontent.com',
+  googleClientId: '115655419099-cuf0d0va85bbvl7ut52krb35ilthp2kd.apps.googleusercontent.com',
   slackClientId: '16724837808.155634746450',
   githubClientId: '469a838ef4c6048510b6'
 };
