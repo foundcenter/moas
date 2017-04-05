6b9eed42cd5fd527d4bd987bf4f857dc1c070907
Nebojsa Vojvodic <neb.vojvodic@gmail.com>
2017-04-05 11:07:59 +0200

    minor fix - use single quotes

--- frontend/src/app/services/search.service.ts
+++ frontend/src/app/services/search.service.ts
@@ -1,8 +1,8 @@
-import { Injectable } from "@angular/core";
-import { Observable } from "rxjs";
-import { JwtHttp } from "ng2-ui-auth";
-import { Response } from "@angular/http";
-import { environment } from "../../environments/environment";
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { JwtHttp } from 'ng2-ui-auth';
+import { Response } from '@angular/http';
+import { environment } from '../../environments/environment';
 
 
 @Injectable()
